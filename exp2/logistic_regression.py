f56b57ff5c83ff276232343e6cb806f77dc7631b
david-zhang-test <1839476290@qq.com>
2025-05-17 22:35:03 +0800

    finish exp2

---
+++ exp2/logistic_regression.py
@@ -0,0 +1,116 @@
+from argparse import Namespace
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import urllib
+from sklearn.linear_model import SGDClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+# 参数
+args = Namespace(
+    seed=1234,
+    data_file="exp2/titanic.csv",
+    train_size=0.75,
+    test_size=0.25,
+    num_epochs=100,
+    max_depth=4,
+    min_samples_leaf=5,
+    n_estimators=10, # 随机森林中包含的决策树个数
+)
+
+# 设置随即种子来保证实验结果的可重复性。
+np.random.seed(args.seed)
+df = pd.read_csv(args.data_file, header=0)
+
+
+# 预处理
+def preprocess(df):
+  
+    # 删除掉含有空值的行
+    df = df.dropna()
+
+    # 删除基于文本的特征 (我们以后的课程将会学习怎么使用它们)
+    features_to_drop = ["name", "cabin", "ticket"]
+    df = df.drop(features_to_drop, axis=1)
+
+    # pclass, sex, 和 embarked 是类别变量
+    categorical_features = ["pclass","embarked","sex"]
+    df = pd.get_dummies(df, columns=categorical_features)
+
+    return df
+
+df = preprocess(df)
+
+
+##### interaction item #####
+def interaction(df):
+    df["age_fare"] = df["age"] * df["fare"]
+    df["pclass1_sex"] = df["pclass_1"] * df["sex_female"]
+    return df
+
+
+
+df = interaction(df)
+print(df.head())
+
+mask = np.random.rand(len(df)) < args.train_size
+train_df = df[mask]
+test_df = df[~mask]
+print ("Train size: {0}, test size: {1}".format(len(train_df), len(test_df)))
+
+
+# 分离 X 和 y
+X_train = train_df.drop(["survived"], axis=1)
+y_train = train_df["survived"]
+X_test = test_df.drop(["survived"], axis=1)
+y_test = test_df["survived"]
+
+print(X_train.head())
+
+# 标准化训练数据 (mean=0, std=1)
+X_scaler = StandardScaler().fit(X_train)
+
+# 标准化训练和测试数据  (不要标准化标签分类y)
+standardized_X_train = X_scaler.transform(X_train)
+standardized_X_test = X_scaler.transform(X_test)
+
+# 检查
+print ("mean:", np.mean(standardized_X_train, axis=0)) # mean 应该为 ~0
+print ("std:", np.std(standardized_X_train, axis=0))   # std 应该为 1
+
+
+
+log_reg = SGDClassifier(loss="log_loss", penalty=None, max_iter=args.num_epochs, 
+                        random_state=args.seed)
+log_reg.fit(X=standardized_X_train, y=y_train)
+pred_test = log_reg.predict_proba(standardized_X_test)
+print (pred_test[:5])
+
+pred_train = log_reg.predict(standardized_X_train) 
+pred_test = log_reg.predict(standardized_X_test)
+print(pred_test)
+
+
+# 正确率
+train_acc = accuracy_score(y_train, pred_train)
+test_acc = accuracy_score(y_test, pred_test)
+print ("train acc: {0:.2f}, test acc: {1:.2f}".format(train_acc, test_acc))
+
+
+coef = log_reg.coef_ / X_scaler.scale_
+intercept = log_reg.intercept_ - np.sum((coef * X_scaler.mean_))
+print (coef)
+print (intercept)
+indices = np.argsort(coef)
+features = list(X_train.columns)
+print ("Features correlated with death:", [features[i] for i in indices[0][:3]])
+print ("Features correlated with survival:", [features[i] for i in indices[0][-3:]])
+
+
+###### odds ratio ######
+coef = np.exp(coef[0])
+print("Odds ratio greater than 1:", [features[i] for i in indices[0] if coef[i] > 1])
+print("Odds ratio less than 1:", [features[i] for i in indices[0] if coef[i] <= 1])
+
+
